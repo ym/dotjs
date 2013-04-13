3c88cba21179e611288e4cc34c958ea60b2b8e69
Aveline Swan <i@vii.im>
2013-04-14 07:07:15 +0800

    Added some torrent download sites

---
+++ 17domn.com.js
@@ -0,0 +1,1 @@
+tenitt.info.js
---
+++ 97down.info.js
@@ -0,0 +1,6 @@
+document.forms[0].onsubmit = function() {};
+document.forms[0].submit();
+window.setTimeout(function(){
+        window.open('', '_self', ''); 
+            window.close();
+}, 1000);
