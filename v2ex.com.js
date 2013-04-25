261b7fed38f413fbd6249f89805958f2095f142b
Aveline Swan <i@vii.im>
2013-04-25 10:54:59 +0800

    Fixed a bug in filtering default avatar

--- v2ex.com.js
+++ v2ex.com.js
@@ -1,14 +1,21 @@
-var avatars = document.getElementsByClassName('avatar');
-for(var i in avatars) {
-	(function (avatar) {
-		if(!avatar.src || avatar.src.indexOf('avatar_') == -1) {
-			return ;
-		}
+(function () {
+	if(document.location.href.indexOf('/member/') !== -1) {
+		return ;
+	}
 
-		if(avatar.parentNode.tagName == 'TD') {
-			avatar.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
-		} else if(avatar.parentNode.tagName == 'A' && avatar.parentNode.parentNode.tagName == 'TD') {
-			avatar.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
-		}
-	})(avatars[i]);
-}
+	var avatars = document.getElementsByClassName('avatar');
+
+	for(var i in avatars) {
+		(function (avatar) {
+			if(!avatar.src || avatar.src.indexOf('avatar_') == -1) {
+				return ;
+			}
+
+			if(avatar.parentNode.tagName == 'TD') {
+				avatar.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+			} else if(avatar.parentNode.tagName == 'A' && avatar.parentNode.parentNode.tagName == 'TD') {
+				avatar.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+			}
+		})(avatars[i]);
+	}
+})();
