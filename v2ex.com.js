ca3c9ccbe3769416f63fbdf30f424625b3fb1946
Aveline Swan <i@vii.im>
2013-04-25 10:50:01 +0800

    Added default avatar filter for V2EX

---
+++ v2ex.com.js
@@ -0,0 +1,14 @@
+var avatars = document.getElementsByClassName('avatar');
+for(var i in avatars) {
+	(function (avatar) {
+		if(!avatar.src || avatar.src.indexOf('avatar_') == -1) {
+			return ;
+		}
+
+		if(avatar.parentNode.tagName == 'TD') {
+			avatar.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+		} else if(avatar.parentNode.tagName == 'A' && avatar.parentNode.parentNode.tagName == 'TD') {
+			avatar.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+		}
+	})(avatars[i]);
+}
